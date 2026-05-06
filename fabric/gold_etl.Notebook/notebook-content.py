4d304c82e4c617dbb1f93dc3f75313957510e1e6
Vladyslav Dzenziur <dzenziur@gmail.com>
2026-05-06 16:35:49 +0200

    feat(gold): add gold_etl notebook

---
+++ fabric/gold_etl.Notebook/notebook-content.py
@@ -0,0 +1,43 @@
+# Fabric notebook source
+
+# METADATA ********************
+
+# META {
+# META   "kernel_info": {
+# META     "name": "synapse_pyspark"
+# META   },
+# META   "dependencies": {
+# META     "lakehouse": {
+# META       "default_lakehouse": "cc456ca8-0bdb-48e0-b22e-523d9267e9c6",
+# META       "default_lakehouse_name": "silver_lakehouse",
+# META       "default_lakehouse_workspace_id": "d5f75821-ae8f-4a0a-b235-74982716aa0b",
+# META       "known_lakehouses": [
+# META         {
+# META           "id": "cc456ca8-0bdb-48e0-b22e-523d9267e9c6"
+# META         }
+# META       ]
+# META     },
+# META     "warehouse": {
+# META       "default_warehouse": "544920bc-713b-90c9-4289-8411de80194d",
+# META       "known_warehouses": [
+# META         {
+# META           "id": "544920bc-713b-90c9-4289-8411de80194d",
+# META           "type": "Datawarehouse"
+# META         }
+# META       ]
+# META     }
+# META   }
+# META }
+
+# CELL ********************
+
+# Welcome to your new notebook
+# Type here in the cell editor to add code!
+
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
